fe6be6abb8df58978df47e645af86919aec9727c
Ureshipan <75073370+Ureshipan@users.noreply.github.com>
2021-03-04 16:57:43 +0300

    Mars 3

--- Mars/mars.py
+++ Mars/mars.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def bruh():
+    return "Миссия Колонизация Марса"
 @app.route('/index')
 def index():
-    return "Привет, Яндекс!"
+    return "И на Марсе будут яблони цвести!"
+@app.route('/promoute')
+def prom():
+    return ("Человечество вырастает из детства.</br>"
+    "Человечеству мала одна планета.</br>"
+    "Мы сделаем обитаемыми безжизненные пока планеты.</br>"
+    "И начнем с Марса!</br>"
+    "Присоединяйся!")
+@app.route('/image_mars')
+def image():
+    return ('''<!doctype html>
+                            <html lang="en">
+                              <head>
+                                <title>Привет, Марс!</title>
+                              </head>
+                              <body>
+                                <h1>Жди нас, Марс!</h1>
+                              </body>
+                            </html>'''
+           f'''<img src="{url_for('static', filename='img/MARS.png')}" 
+           alt="здесь должна была быть картинка, но не нашлась">''')
+            
 
 
 if __name__ == '__main__':
